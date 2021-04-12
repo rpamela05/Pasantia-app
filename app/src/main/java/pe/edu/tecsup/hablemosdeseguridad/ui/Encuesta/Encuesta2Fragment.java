efce4103b31a3c9261f9530417fe3075720e2713
nyagua <nelson.yagua@tecsup.edu.pe>
2021-04-12 12:05:46 -0500

    Youtube video player and Spinner Firebase

--- app/src/main/java/pe/edu/tecsup/hablemosdeseguridad/ui/Encuesta/Encuesta2Fragment.java
+++ app/src/main/java/pe/edu/tecsup/hablemosdeseguridad/ui/Encuesta/Encuesta2Fragment.java
@@ -2,16 +2,29 @@ package pe.edu.tecsup.hablemosdeseguridad.ui.Encuesta;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pe.edu.tecsup.hablemosdeseguridad.R;
+import pe.edu.tecsup.hablemosdeseguridad.model.Video;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +32,12 @@ import pe.edu.tecsup.hablemosdeseguridad.R;
  * create an instance of this fragment.
  */
 public class Encuesta2Fragment extends Fragment {
+
+    // Data Spinner Firebase
+    Spinner mSpinnerVideos;
+    DatabaseReference mDatabase;
+
+    ArrayList<String> arrayList = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -64,8 +83,15 @@ public class Encuesta2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_encuesta2, container, false);
+
+        // Show database spinner Firebase
+        mSpinnerVideos = v.findViewById(R.id.spinner_video);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        showDatabaseSpinner();
+
         btnSgte=v.findViewById(R.id.btnSiguiente);
         btnSgte.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,4 +105,30 @@ public class Encuesta2Fragment extends Fragment {
         });
         return v;
     }
+
+    public void showDatabaseSpinner() {
+        final List<Video> videos = new ArrayList<>();
+        mDatabase.child("Video").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot ds: snapshot.getChildren()) {
+                            String id = ds.getKey();
+                            String title = ds.child("title").getValue().toString();
+                            String videoId = ds.child("videoId").getValue().toString();
+                            videos.add(new Video(id, title, videoId));
+                    }
+
+                    ArrayAdapter<Video> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, videos);
+                    mSpinnerVideos.setAdapter(arrayAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/pe/edu/tecsup/hablemosdeseguridad/ui/Encuesta/EncuestaFragment.java
+++ app/src/main/java/pe/edu/tecsup/hablemosdeseguridad/ui/Encuesta/EncuestaFragment.java
@@ -1,28 +1,32 @@
 package pe.edu.tecsup.hablemosdeseguridad.ui.Encuesta;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 import pe.edu.tecsup.hablemosdeseguridad.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EncuestaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EncuestaFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -36,20 +40,10 @@ public class EncuestaFragment extends Fragment {
     private TextView txt;
     private MaterialButton btnsgte;
 
-
-
     public EncuestaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EnncuestaFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static EncuestaFragment newInstance(String param1, String param2) {
         EncuestaFragment fragment = new EncuestaFragment();
@@ -72,6 +66,7 @@ public class EncuestaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         SharedPreferences preferences=getActivity().getSharedPreferences("Credencial", 0);
         String dni=preferences.getString("dni","");
         View v= inflater.inflate(R.layout.fragment_encuesta, container, false);
@@ -88,4 +83,5 @@ public class EncuestaFragment extends Fragment {
         });
         return v;
     }
+
 }
